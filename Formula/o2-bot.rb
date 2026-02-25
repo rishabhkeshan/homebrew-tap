46d315184737c9a8e07c78a89c96279b6ad9a6bd
rishabhkeshan <rishabhkeshan@gmail.com>
2026-02-25 18:23:59 +0530

    Update formula with v1.0.0 checksums and fix workflow permissions

--- Formula/o2-bot.rb
+++ Formula/o2-bot.rb
@@ -7,14 +7,14 @@ class O2Bot < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/rishabhkeshan/o2-cli-bot/releases/download/v#{version}/o2-bot-macos-arm64"
-      sha256 "PLACEHOLDER"
+      sha256 "0516f23d2865758c7741e5bdd9891d5623f9d52998ccad0b12abc72dd192442b"
 
       def install
         bin.install "o2-bot-macos-arm64" => "o2-bot"
       end
     else
       url "https://github.com/rishabhkeshan/o2-cli-bot/releases/download/v#{version}/o2-bot-macos-x64"
-      sha256 "PLACEHOLDER"
+      sha256 "8da0106de65a19490aa5f95c6954f3002a2ba7b5130f291c56f37c8717ea09b6"
 
       def install
         bin.install "o2-bot-macos-x64" => "o2-bot"
@@ -24,7 +24,7 @@ class O2Bot < Formula
 
   on_linux do
     url "https://github.com/rishabhkeshan/o2-cli-bot/releases/download/v#{version}/o2-bot-linux-x64"
-    sha256 "PLACEHOLDER"
+    sha256 "ae3c4c76b212159738ebe243dd90fee40c89afadf29ec659f29cfb36783d60a4"
 
     def install
       bin.install "o2-bot-linux-x64" => "o2-bot"
